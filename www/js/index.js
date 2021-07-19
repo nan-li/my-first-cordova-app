c926db7413e9ebe1a6eaf48087aa3f1ea1262ab3
Nan <nanftw@gmail.com>
2021-07-19 16:43:55 -0700

    Prompt for push and sets language to 'fr'

--- www/js/index.js
+++ www/js/index.js
@@ -26,6 +26,11 @@ function onDeviceReady() {
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
-    window.plugins.OneSignal.setAppId("0954e505-67a2-4e29-a052-9679afca8e6b");
+    // window.plugins.OneSignal.setAppId("0954e505-67a2-4e29-a052-9679afca8e6b");
+    
+    window.plugins.OneSignal.setAppId("77e32082-ea27-42e3-a898-c72e141824ef");
+    window.plugins.OneSignal.promptForPushNotificationsWithUserResponse(function(accepted) {
+        console.log("User accepted notifications: " + accepted);
+    });
     window.plugins.OneSignal.setLanguage("fr");
 }
